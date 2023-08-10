ac5d2fdcae76cf4aff5cc1cb47e58efe8f30662c
selim <salimmia3745@gmail.com>
2023-08-10 16:49:14 +0600

    Working on the Movies component (31) with linking movie title

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,11 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Movies() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    let moviesList = [
+      {
+        id: 1,
+        title: "Priotoma",
+        release_date: "2023-03-03",
+        runtime: 116,
+        mpaa_rating: "R",
+        sescription: "Some long description",
+      },
+      {
+        id: 2,
+        title: "Avengers",
+        release_date: "2021-03-03",
+        runtime: 115,
+        mpaa_rating: "PG-13",
+        sescription: "Some long description",
+      },
+    ];
+    setMovies(moviesList);
+  }, []);
+
   return (
     <>
       <div className="text-center">
         <h2>Movies</h2>
         <hr />
+        <table className="table table-striped table-hover">
+          <thead>
+            <tr>
+              <th>Movie</th>
+              <th>Release Date</th>
+              <th>Rating</th>
+            </tr>
+          </thead>
+          <tbody>
+            {movies.map((movie) => (
+              <tr key={movie.id}>
+                <td>
+                  <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+                </td>
+                <td>{movie.release_date}</td>
+                <td>{movie.mpaa_rating}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </>
   );
